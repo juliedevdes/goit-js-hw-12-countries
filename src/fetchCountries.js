73bc4c35df2729302f25dcf60082b3e4702c003a
Julia Ohrimenko <juliajam2497@gmail.com>
2021-08-15 15:40:36 +0300

    made fetch function more clear

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,17 +1,8 @@
 // Есть файл fetchCountries.js с дефолтным экспортом функции
 // fetchCountries(searchQuery), возвращающей промис с массивом стран, результат запроса к API.
 
-import countryCard from '../src/country-card.hbs';
-
-fetch('https://restcountries.eu/rest/v2/name/Ukraine')
-  .then(response => {
-    return response.json();
-  })
-  .then(country => {
-    const markup = countryCard(country[0]);
-    const card = document.querySelector('.js-country-container');
-    card.innerHTML = markup;
-  })
-  .catch(error => {
-    console.log(error);
-  });
+export default function fetchCountries(searchQuery) {
+  return fetch(`https://restcountries.eu/rest/v2/name/${searchQuery}`).then(response =>
+    response.json(),
+  );
+}
--- src/index.js
+++ src/index.js
@@ -1,1 +1,24 @@
-import fetchCountry from './fetchCountries';
+import countryCard from '../src/country-card.hbs'; // handlebars
+
+import fetchCountries from './fetchCountries'; // fetch func
+
+//references
+
+const input = document.querySelector('.input');
+const container = document.querySelector('.container');
+
+fetchCountries('POrtugal')
+  .then(renderOneCountryCard)
+  .catch(error => console.log(error));
+
+//render one country
+function renderOneCountryCard(country) {
+  const markup = countryCard(country[0]);
+  container.innerHTML = markup;
+}
+
+//render list of contries
+
+//eventList
+
+//
